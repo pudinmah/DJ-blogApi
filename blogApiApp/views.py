97d3334b90e8d79be8d289252133399940912629
Mahpudin <pudinmah69@gmail.com>
2024-02-18 06:54:01 +0700

    Creating First Endpoint

--- blogApiApp/views.py
+++ blogApiApp/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+
 
 # Create your views here.
+
+
+@api_view(['GET'])
+def index(request):
+
+
+    return Response({"Success":"The setup was successfully"})
